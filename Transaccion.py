f38d34738c0ea660a15f595605ee3cbde8273e2d
asb160 <asb160@alu.ua.es>
2026-03-20 13:18:47 +0100

    Clase abstracta en Transaccion

--- Transaccion.py
+++ Transaccion.py
@@ -1,12 +1,15 @@
-class Transaccion:
+from abc import ABC, abstractmethod
+
+class Transaccion(ABC):
     def __init__(self, concepto, importe, categoria, fecha):
         if importe <= 0:
-            print("Importe invalido")
-            return
+            raise ValueError("El importe debe ser estrictamente mayor que 0.")
+
         self.concepto = concepto
         self.importe = importe
         self.categoria = categoria
         self.fecha = fecha
 
+    @abstractmethod
     def mostrar(self):
         return f'[{self.fecha}] {self.concepto}: {self.importe}'
